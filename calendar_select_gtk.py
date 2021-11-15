187fb519313c47fe73a78cc39feb09aa79754adc
confodere <l.tuppercreed@gmail.com>
2021-11-15 19:18:06 +1100

    implemented dialogue window

--- calendar_select_gtk.py
+++ calendar_select_gtk.py
@@ -4,8 +4,47 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio, GLib
 
+from cal import Calendar, Calendars
 
-from calendar_select import sql_calendar
+
+class DialogCal(Gtk.Dialog):
+    def __init__(self, parent, calendars):
+        super().__init__(title="Select active calendars", transient_for=parent, flags=0)
+        self.set_border_width(10)
+
+        self.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK)
+
+        box = self.get_content_area()
+
+        box.set_spacing(6)
+
+        header = Gtk.Label(label="Selected Calendars", xalign=0)
+        box.pack_start(header, True, True, 0)
+
+        listbox = Gtk.ListBox()
+        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        box.pack_start(listbox, True, True, 0)
+
+        self.switches = {}
+
+        for calendar in calendars:
+            row = Gtk.ListBoxRow()
+            row.set_margin_start(5)
+            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=25)
+            row.add(hbox)
+            label = Gtk.Label(label=calendar.name, xalign=0)
+            label.set_max_width_chars(15)
+            label.set_line_wrap(True)
+            switch = Gtk.Switch()
+            switch.set_active(calendar.active)
+            hbox.pack_start(label, True, True, 0)
+            hbox.pack_start(switch, False, True, 0)
+
+            self.switches[calendar.id] = switch
+
+            listbox.add(row)
+
+        self.show_all()
 
 
 class MyWindow(Gtk.Window):
@@ -18,55 +57,51 @@ class MyWindow(Gtk.Window):
         box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(box_outer)
 
-        listbox = Gtk.ListBox()
-        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
-        box_outer.pack_start(listbox, True, True, 0)
+        button = Gtk.Button(label="open dialog")
+        button.connect("clicked", self.on_button_clicked)
+        box_outer.pack_start(button, True, True, 0)
 
-        row = Gtk.ListBoxRow()
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-        row.add(hbox)
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        hbox.pack_start(vbox, True, True, 0)
+    def on_button_clicked(self, widget):
+        calendars = Calendars()
 
-        label1 = Gtk.Label("asdfsf", xalign=0)
-        label2 = Gtk.Label("label 2", xalign=0)
-        vbox.pack_start(label1, True, True, 0)
-        vbox.pack_start(label2, True, True, 0)
+        dialog = DialogCal(self, calendars)
+        response = dialog.run()
 
-        switch = Gtk.Switch()
-        switch.props.valign = Gtk.Align.CENTER
-        hbox.pack_start(switch, False, True, 0)
+        if response == Gtk.ResponseType.OK:
+            print("Ok")
+            results = {key: switch.get_active() for key, switch in dialog.switches.items()}
 
-        # listbox.add(row)
+            overwrites = []
+            for i, cal in enumerate(calendars):
+                if cal.active != results[cal.id]:
+                    overwrites.append(
+                        (
+                            i,
+                            Calendar(
+                                id=cal.id,
+                                name=cal.name,
+                                description=cal.description,
+                                time_zone=cal.time_zone,
+                                active=results[cal.id],
+                            ),
+                        )
+                    )
 
-        cal = sql_calendar()
-        names = cal.read()
+            for overwrite in overwrites:
+                calendars[overwrite[0]] = overwrite[1]
+            if len(overwrites) > 0:
+                calendars.write()
+        elif response == Gtk.ResponseType.CANCEL:
+            print("cancel")
 
-        for name in names:
-            row = Gtk.ListBoxRow()
-            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-            row.add(hbox)
-            label = Gtk.Label(name.summary, xalign=0)
-            switch = Gtk.Switch()
-            hbox.pack_start(label, True, True, 0)
-            hbox.pack_start(switch, False, True, 0)
+        dialog.destroy()
 
-            listbox.add(row)
-
-        def sort_func(row_1, row_2, data, notify_destroy):
+        """def sort_func(row_1, row_2, data, notify_destroy):
             text_1 = row_1.get_child().get_children()[0].get_text()
             text_2 = row_2.get_child().get_children()[0].get_text()
             return text_1 > text_2
 
-        listbox.set_sort_func(sort_func, None, False)
-
-        # label = Gtk.Label(label="Hello World", angle=25, halign=Gtk.Align.END)
-
-        # widget = Gtk.Box()
-        # print("fs")
-
-    def on_button_clicked(self, widget):
-        print("Hello, World")
+        listbox.set_sort_func(sort_func, None, False)"""
 
 
 class Application(Gtk.Application):
