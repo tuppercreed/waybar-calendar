ca279f539c85c98ed01baffb84ee83bdd07f36d9
confodere <l.tuppercreed@gmail.com>
2021-11-15 22:09:19 +1100

    utilises new events object

--- check_db.py
+++ check_db.py
@@ -1,8 +1,9 @@
-import pytz
-import sqlite3
 from datetime import datetime, date, time, timedelta
-import sys, os
-import json
+import sys, os, json, sqlite3
+
+import pytz
+
+from cal import Events, Event
 
 SEARCH_DATE = "2021-06-29"
 
@@ -25,34 +26,20 @@ def find_time_bound(local_tz, custom_search=False):
     return start, end
 
 
-def read(start, end):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    path = f"{dir_path}/cal.db"
-    con = sqlite3.connect(path, detect_types=sqlite3.PARSE_COLNAMES)
-    cur = con.cursor()
-
-    results = cur.execute(
-        "SELECT start AS 'start [datetime]', end AS 'end [datetime]', title, active FROM events INNER JOIN calendars ON events.calendar_id = calendars.id WHERE start BETWEEN ? AND ? AND type = 'timed' AND active = 1 ORDER BY start LIMIT 1",
-        (start, end),
-    )
-    return list(results)
-
-
 def main():
     local_tz = pytz.timezone("Australia/Melbourne")
+    tz_name = "Australia/Melbourne"
     start, end = find_time_bound(local_tz, custom_search=False)
-    data = read(start, end)
+
+    data = Events(window=(start, end))
 
     if len(data) == 1:
-        event_list = data[0]
+        event = data[0]
         info = {
-            "start": event_list[0].astimezone(local_tz).strftime("%Y-%m-%d"),
-            "end": event_list[1].astimezone(local_tz).strftime("%Y-%m-%d"),
-            "title": event_list[2],
-            # "time_diff": int(
-            #    (event_list[0] - local_tz.localize(datetime.now()).astimezone(pytz.utc)).total_seconds() / 60
-            # ),
-            "time_diff": int((event_list[0] - start).total_seconds() / 60),
+            "start": event.local_start(tz_name).strftime("%H:%M"),
+            "end": event.local_end(tz_name).strftime("%H:%M"),
+            "title": event.name,
+            "time_diff": int((event.start - start).total_seconds() / 60),
         }
 
         if info["time_diff"] > 60:
@@ -69,7 +56,7 @@ def main():
         d = {
             "text": f"Next event: {info['title']} starting in {upcoming}",
             "alt": "Alt-text",
-            "tooltip": "This is a toooltip",
+            "tooltip": f"Start: {info['start']} \n End: {info['end']}",
         }
 
         sys.stdout.write(json.dumps(d))
