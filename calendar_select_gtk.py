d30af39bb11c87f88e0782d9052d9e676e99ac50
confodere <l.tuppercreed@gmail.com>
2021-11-24 00:38:37 +1100

    sync buttons and event display organisation

--- calendar_select_gtk.py
+++ calendar_select_gtk.py
@@ -1,4 +1,4 @@
-import sys
+import sys, importlib
 from datetime import datetime, timedelta
 
 import gi
@@ -7,6 +7,8 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio, GLib
 
 from cal import Calendar, Calendars, Event, Events
+
+google_calendar = importlib.import_module("google_calendar", "waybar-calendar")
 
 TZ_NAME = "Australia/Melbourne"
 
@@ -31,7 +33,7 @@ class DialogCal(Gtk.Dialog):
 
         self.switches = {}
 
-        for calendar in calendars:
+        for calendar in calendars.values():
             row = Gtk.ListBoxRow()
             row.set_margin_start(5)
             hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=25)
@@ -65,44 +67,77 @@ class MyWindow(Gtk.Window):
 
         self.set_resizable(False)
 
+        self._calendars = Calendars()
+
         box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(box_outer)
 
         start = datetime.utcnow()
         end = start + timedelta(150)
-        events = Events(window=(start, end), limit=5)
+        events = Events(window=(start, end), limit=10)
 
         blurb = Gtk.Label(label="Upcoming events:", xalign=0)
         box_outer.pack_start(blurb, True, True, 0)
 
-        for event in events:
-            box_event = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
+        for group, events in events.group(TZ_NAME).items():
+            dt = events[0].local_start(TZ_NAME)
+            if 4 <= dt.day % 100 <= 20:
+                day_tag = "th"
+            else:
+                day_tag = {1: "st", 2: "nd", 3: "rd"}.get(dt.day % 10, "th")
+            group_label = f"{dt:%A}, {dt:%-d}{day_tag} of {dt:%B}"
+            frame = Gtk.Frame(label=group_label)
+            frame.set_shadow_type(Gtk.ShadowType.ETCHED_IN)
+            box_event_group = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
 
-            widgets = []
-            widgets.append(Gtk.Label(label=event.name, xalign=0))
-            widgets.append(Gtk.Label(label=event.local_start(TZ_NAME).strftime("%Y-%m-%d"), xalign=0))
-            if type(event.start) is datetime:
-                widgets.append(
-                    Gtk.Label(
-                        label=f"{event.local_start(TZ_NAME).strftime('%H:%M')} - {event.local_end(TZ_NAME).strftime('%H:%M')}",
-                        xalign=0,
+            # widgets_group.append(Gtk.Label(label=group, xalign=0))
+
+            for event in events:
+
+                box_event = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=1)
+                box_event.set_margin_start(10)
+                box_event.set_margin_end(10)
+
+                widgets = []
+                widgets.append(Gtk.Label(label=event.name, xalign=0))
+                # widgets.append(Gtk.Label(label=event.local_start(TZ_NAME).strftime("%Y-%m-%d"), xalign=0))
+                if type(event.start) is datetime:
+                    widgets.append(
+                        Gtk.Label(
+                            label=f"{event.local_start(TZ_NAME).strftime('%H:%M')} - {event.local_end(TZ_NAME).strftime('%H:%M')}",
+                            xalign=1,
+                        )
                     )
-                )
-            if event.description is not None:
-                widgets.append(Gtk.Label(label=event.description, xalign=0))
+                if event.description is not None:
+                    widgets.append(Gtk.Label(label=event.description, xalign=0))
 
-            for widget in widgets:
-                box_event.pack_start(widget, True, True, 0)
-            box_outer.pack_start(box_event, True, True, 5)
+                for widget in widgets:
+                    box_event.pack_start(widget, True, True, 0)
 
-        button = Gtk.Button(label="Configure Calendars")
-        button.connect("clicked", self.on_button_clicked)
-        box_outer.pack_start(button, True, True, 0)
+                box_event_group.pack_start(box_event, True, True, 0)
 
-    def on_button_clicked(self, widget):
-        calendars = Calendars()
+            frame.add(box_event_group)
+            box_outer.pack_start(frame, True, True, 5)
 
-        dialog = DialogCal(self, calendars)
+        box_buttons = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=3)
+
+        button_configure = Gtk.Button(label="Configure Calendars")
+        button_configure.connect("clicked", self.on_configure_clicked)
+        box_buttons.pack_start(button_configure, True, True, 0)
+
+        button_sync_calendars = Gtk.Button(label="Sync Calendars")
+        button_sync_calendars.connect("clicked", self.on_sync_clicked)
+        box_buttons.pack_start(button_sync_calendars, True, True, 0)
+
+        button_sync_events = Gtk.Button(label="Sync Events")
+        button_sync_events.connect("clicked", self.on_sync_events_clicked)
+        box_buttons.pack_start(button_sync_events, True, True, 0)
+
+        box_outer.pack_start(box_buttons, True, True, 0)
+
+    def on_configure_clicked(self, widget):
+
+        dialog = DialogCal(self, self._calendars)
         response = dialog.run()
 
         if response == Gtk.ResponseType.OK:
@@ -110,7 +145,7 @@ class MyWindow(Gtk.Window):
             results = {key: switch.get_active() for key, switch in dialog.switches.items()}
 
             overwrites = []
-            for i, cal in enumerate(calendars):
+            for i, cal in self._calendars.items():
                 if cal.active != results[cal.id]:
                     overwrites.append(
                         (
@@ -126,9 +161,9 @@ class MyWindow(Gtk.Window):
                     )
 
             for overwrite in overwrites:
-                calendars[overwrite[0]] = overwrite[1]
+                self._calendars[overwrite[0]] = overwrite[1]
             if len(overwrites) > 0:
-                calendars.write()
+                self._calendars.write()
         elif response == Gtk.ResponseType.CANCEL:
             print("cancel")
 
@@ -140,6 +175,34 @@ class MyWindow(Gtk.Window):
             return text_1 > text_2
 
         listbox.set_sort_func(sort_func, None, False)"""
+
+    def on_sync_clicked(self, widget):
+        dialog = Gtk.MessageDialog(
+            transient_for=self,
+            flags=0,
+            message_type=Gtk.MessageType.INFO,
+            buttons=Gtk.ButtonsType.OK,
+            text="Completed Sync",
+        )
+
+        cals = google_calendar.sync_calendars()
+
+        dialog.run()
+        dialog.destroy()
+
+    def on_sync_events_clicked(self, widget):
+        dialog = Gtk.MessageDialog(
+            transient_for=self,
+            flags=0,
+            message_type=Gtk.MessageType.INFO,
+            buttons=Gtk.ButtonsType.OK,
+            text="Completed Sync",
+        )
+
+        events = google_calendar.sync_events(self._calendars)
+
+        dialog.run()
+        dialog.destroy()
 
 
 class Application(Gtk.Application):
